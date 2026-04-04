00a5899b44aa8175ed6f4407d3fcfd7ec25ba91c
Ufuk Cetinkaya <ufuk.cetinkya@gmail.com>
2026-04-04 14:17:16 +0300

    ingress and kustomization added

--- shell/install-nginx.sh
+++ shell/install-nginx.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
 helm repo update
-helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
 
-openssl req -x509 -nodes -days 365 \
--newkey rsa:2048 \
--keyout tls.key \
--out tls.crt \
--subj "/CN=*.ufukcetinkaya.net/O=dev"
-
-kubectl create secret tls api-tls --cert=tls.crt --key=tls.key -n edonusum
-
-kubectl apply -f ../k8s/ingress.yaml -n edonusum
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
