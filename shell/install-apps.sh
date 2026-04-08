2707562c1481c2b741d92dc165c1b3ebb6877880
Ufuk Cetinkaya <ufuk.cetinkya@gmail.com>
2026-04-08 17:28:31 +0300

    api-gateway and auth-api helm install commands added

--- shell/install-apps.sh
+++ shell/install-apps.sh
@@ -2,6 +2,8 @@
 
 cd ../chart
 
+helm install api-gateway . -f values-api-gateway.yaml --namespace api-gateway --create-namespace
+helm install auth-api . -f values-auth-api.yaml --namespace auth-api --create-namespace
 helm install signer-ws . -f values-signer-ws.yaml --namespace signer-ws --create-namespace
 helm install gibuser-worker . -f values-gibuser-worker.yaml --namespace gibuser-worker --create-namespace
 helm install gibuser-api . -f values-gibuser-api.yaml --namespace gibuser-api --create-namespace
